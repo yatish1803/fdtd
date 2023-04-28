79d24264dc7b405113015fa6b1a0ea5d51aedad0
yatish1803-Laptop <yatish4science@gmail.com>
2023-04-28 18:02:13 +0200

    multiple sphere case is working

--- Scripts/multiple_spheres.py
+++ Scripts/multiple_spheres.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 # print("Switched to:",matplotlib.get_backend())
 # from mayavi import mlab
 
-sx = 256
-sy = 512
-sz = 512
+sx = 200
+sy = 150
+sz = 256
 
 dim_x = 100
 dim_y = 100
@@ -45,14 +45,15 @@ fdtd.set_backend("numpy")
 
 # In[3]:
 
-wavelength = 491e-9
+wavelength_freespace = 491e-9
+vel_light: float = 299_792_458.0  # [m/s] speed of light
 n_m = 1.38
 n_s = 1.33
-
-factor = 10
+wavelength = wavelength_freespace / n_s
+wavelength_px = 10
 grid = fdtd.Grid(
-    shape=(sx+40, sy+30, 1),  # 25um x 15um x 1 (grid_spacing) --> 2D FDTD
-    grid_spacing=(wavelength / n_m) / factor,
+    shape=(sx + 40, sy + 30, 1),  # 25um x 15um x 1 (grid_spacing) --> 2D FDTD
+    grid_spacing=(wavelength_freespace / n_m) / wavelength_px,  # from Nyquist Theorem
     permittivity=n_s ** 2,
 )
 
@@ -64,20 +65,22 @@ grid[:, -10:, :] = fdtd.PML(name="pml_yhigh")
 # simfolder = grid.save_simulation("Single_Sphere")  # initializing environment to save simulation data
 # print(simfolder)
 
-grid[0:sx+40, 10, 0] = fdtd.LineSource(
-    period=wavelength / 3e8, name="source"
+grid[0:sx + 40, 10, 0] = fdtd.LineSource(
+    period=wavelength_freespace / vel_light, name="source"
 )
 
-grid[20:sx+20, 10:sy+10, 0] = fdtd.BlockDetector(name="detector")
+grid[20:sx + 20, 10:sy + 10, 0] = fdtd.BlockDetector(name="detector")
 
 # x = y = np.linspace(-1, 1, 100)
 # X, Y = np.meshgrid(x, y)
 # circle_mask = X**2 + Y**2 < 1
+gap_x = 10
+gap_y = 10
 permittivity = np.ones((100, 100, 1)) * n_s ** 2
 permittivity += slice2D[:, :, None] * (n_m ** 2 - n_s ** 2)
-grid[100:200, 100:200, 0] = fdtd.Object(permittivity=permittivity, name="object")
+grid[20+gap_x:20+gap_x+permittivity.shape[0], 10+gap_y:10+gap_y+permittivity.shape[1], 0] = fdtd.Object(permittivity=permittivity, name="object")
 
-grid.run(total_time=3)
+grid.run(total_time=200)
 # grid.save_data()  # saving detector readings
 grid.visualize(z=0, show=True)
 # plt.show()
