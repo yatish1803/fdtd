b5b45bed9046a9da7fa67090d107c9885ab68a0a
yatish1803 <99249114+yatish1803@users.noreply.github.com>
2023-06-20 18:01:27 +0200

    small mistake was corrected
    pml2s_y_left was changed to pml2s_z_left

--- Scripts/multiple_spheres.py
+++ Scripts/multiple_spheres.py
@@ -52,39 +52,40 @@ grid[b2s:b2pml + pml2d + sx + pml2d + b2pml - b2s, 10, 0] = fdtd.LineSource(
 
 #################### for multiple spheres #################
 ######################## make simulation volume ############
-dim_x = 150
-dim_z = 270
-dim_y = 150
+# dim_x = 150
+# dim_z = 270
+# dim_y = 150
 
-volume_fraction = 0.15
-diameter_spheres_um = 1.0
-diameter_spheres = int(diameter_spheres_um*1e-6 / grid_spacing)
-print(diameter_spheres)
-volume = new_func.generate_Volume_Binary(diameter_spheres, volume_fraction, dim_x, dim_z, dim_y, folder=simfolder)
-################################################################
-object_x_ini = int(b2pml + pml2d + sx/2 - dim_x/2)
-object_x_fin = int(object_x_ini + dim_x)
-gap_y = 10
-slice2D = volume[:, :, dim_y // 2]
-permittivity = np.ones((dim_x, dim_z, 1)) * n_s ** 2
-permittivity += slice2D[:, :, None] * (n_m ** 2 - n_s ** 2)
-grid[object_x_ini:object_x_fin, b2pml + pml2s_z_left + s2d_left + gap_y:b2pml + pml2s_z_left + s2d_left + gap_y + permittivity.shape[1], 0] = fdtd.Object(permittivity=permittivity, name="object")
+# volume_fraction = 0.15
+# diameter_spheres_um = 1.0
+# diameter_spheres = int(diameter_spheres_um*1e-6 / grid_spacing)
+# print(diameter_spheres)
+# volume = new_func.generate_Volume_Binary(diameter_spheres, volume_fraction, dim_x, dim_z, dim_y, folder=simfolder)
+# ################################################################
+# object_x_ini = int(b2pml + pml2d + sx/2 - dim_x/2)
+# object_x_fin = int(object_x_ini + dim_x)
+# gap_y = 10
+# slice2D = volume[:, :, dim_y // 2]
+# permittivity = np.ones((dim_x, dim_z, 1)) * n_s ** 2
+# permittivity += slice2D[:, :, None] * (n_m ** 2 - n_s ** 2)
+# grid[object_x_ini:object_x_fin, b2pml + pml2s_z_left + s2d_left + gap_y:b2pml + pml2s_z_left + s2d_left \
+#      + gap_y + permittivity.shape[1], 0] = fdtd.Object(permittivity=permittivity, name="object")
 ##################################################################
 ################## for single sphere #########################
-# diameter_sphere_um = 3.74
-# diameter_sphere = int(3.74e-6 / grid_spacing)
-# print(diameter_sphere)
-# x = y = np.linspace(-1, 1, diameter_sphere)
-# X, Y = np.meshgrid(x, y)
-# circle_mask = X**2 + Y**2 < 1
-# permittivity = np.ones((diameter_sphere, diameter_sphere, 1)) * n_s ** 2
-# permittivity += circle_mask[:, :, None] * (n_m ** 2 - n_s ** 2)
-#
-# object_x_ini = int(b2pml + pml2d + sx/2 - permittivity.shape[0]/2)
-# object_x_fin = int(object_x_ini + permittivity.shape[0])
-# gap_y = 10
-# grid[object_x_ini:object_x_fin, b2pml + pml2s_y_left + s2d_left + gap_y:b2pml + pml2s_y_left + s2d_left + gap_y + permittivity.shape[1], 0] \
-# = fdtd.Object(permittivity=permittivity, name="object")
+diameter_sphere_um = 3.74
+diameter_sphere = int(3.74e-6 / grid_spacing)
+print(diameter_sphere)
+x = y = np.linspace(-1, 1, diameter_sphere)
+X, Y = np.meshgrid(x, y)
+circle_mask = X**2 + Y**2 < 1
+permittivity = np.ones((diameter_sphere, diameter_sphere, 1)) * n_s ** 2
+permittivity += circle_mask[:, :, None] * (n_m ** 2 - n_s ** 2)
+
+object_x_ini = int(b2pml + pml2d + sx/2 - permittivity.shape[0]/2)
+object_x_fin = int(object_x_ini + permittivity.shape[0])
+gap_y = 10
+grid[object_x_ini:object_x_fin, b2pml + pml2s_z_left + s2d_left + gap_y:b2pml + pml2s_z_left + s2d_left \
+     + gap_y + permittivity.shape[1], 0] = fdtd.Object(permittivity=permittivity, name="object")
 ####################################################################
 
 num_bounces = 5
